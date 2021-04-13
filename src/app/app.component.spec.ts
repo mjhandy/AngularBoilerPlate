1df4a927197ec109c683a6c2f6f3e65d41801e8e
Mark Handy <mjhandy@outlook.com>
2021-04-13 14:33:28 -0400

    * minor updates * initial implementation of sassDocs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'ngBoilerPlate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngBoilerPlate');
+    expect(app.title).toEqual('Angular Boiler Plate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngBoilerPlate app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular Boiler Plate app is running!');
   });
 });
